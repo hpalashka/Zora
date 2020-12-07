adff1fc32c5ef83c1acb464070b0fd302c3fc260
hpalashka <hristina.palashka@gmail.com>
2020-12-07 16:01:10 +0200

    Updated Container Tests. Cleaned up yml files

--- Tests/ContainerTests.ps1
+++ Tests/ContainerTests.ps1
@@ -3,9 +3,22 @@ do {
     $count++
     Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
     
-    $testStart = Invoke-WebRequest -Uri http://localhost:5001 -UseBasicParsing
+    $webStart = Invoke-WebRequest -Uri http://localhost:5001 -UseBasicParsing
+    $identityStart = Invoke-WebRequest -Uri http://localhost:5005 -UseBasicParsing
+    $studentsStart = Invoke-WebRequest -Uri http://localhost:5003 -UseBasicParsing
+    $paymentsStart = Invoke-WebRequest -Uri http://localhost:5011 -UseBasicParsing
+    $statisticsStart = Invoke-WebRequest -Uri http://localhost:5013 -UseBasicParsing
+    $notificationsStart = Invoke-WebRequest -Uri http://localhost:5017 -UseBasicParsing
+    $outstandingpaymentsStart = Invoke-WebRequest -Uri http://localhost:5009 -UseBasicParsing
+
+    Write-Output "Tests results:"
+    Write-Output "Identity"  $identityStart.statusCode
     
-    if ($testStart.statuscode -eq '200') {
+    if ($webStart.statuscode -eq '200' -and $identityStart.statuscode -eq '200' -and
+        $studentsStart.statuscode -eq '200' -and $paymentsStart.statuscode -eq '200' -and
+        $statisticsStart.statuscode -eq '200' -and $notificationsStart.statuscode -eq '200' -and
+        $outstandingpaymentsStart.statuscode -eq '200')
+         {
         $started = $true
     } else {
         Start-Sleep -Seconds 5
@@ -16,3 +29,7 @@ do {
 if (!$started) {
     exit 1
 }
+
+
+
+
